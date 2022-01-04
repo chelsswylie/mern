4925c45874c6e09a3d3e9e804c9482a7bdccd8ac
Chelsea <chelsswylie@gmail.com>
2022-01-04 11:56:14 -0500

    backend is complete with API endpoints, tested in postman and confirmed to be working in Mongo

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,6 +5,7 @@ let User = require("../models/user.model");
 //
 // the first endpoint that handles HTTP Get requests
 router.route("/").get((req, res) => {
+  // User.find below is a mongoose function
   User.find()
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error: " + err));
